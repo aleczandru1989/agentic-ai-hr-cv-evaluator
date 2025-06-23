0060509b7b0744fa87aff70878ae926796a2f67c
Alexandru <alexandru.nistor.89@gmail.com>
2025-06-23 21:04:20 +0300

    Implemented cv evaluator

--- app.py
+++ app.py
@@ -7,5 +7,8 @@ load_dotenv(override=True)
 hrEvaluator = HREvaluatorService()
 
 gr.ChatInterface(hrEvaluator.chat,
-                 textbox=gr.MultimodalTextbox(file_types=[".pdf"]),
+                 textbox=gr.MultimodalTextbox(
+                     file_types=[".pdf"],
+                     label="Candidate CV",
+                     placeholder="Please upload a candidate CV as a PDF file"),
                  type="messages").launch()
--- services/hr_evaluator.py
+++ services/hr_evaluator.py
@@ -1,3 +1,4 @@
+from docling.document_converter import DocumentConverter
 from openai import OpenAI
 
 from const.prompts import Prompt
@@ -9,9 +10,14 @@ class HREvaluatorService():
 
 
     def chat(self, message, history):
-        company_profile = self.get_company_profile()
+        # if message["files"] is None or len(message["files"]) == 0:
+        #     return "Please upload a candidate CV as a PDF file"
 
-        messages = [{"role": "system", "content": Prompt.get_system_prompt(company_profile)}] + history + [{"role": "user", "content": message}]
+        system_prompt = self.get_system_prompt()
+
+        user_prompt = self.get_user_message(message)
+
+        messages = [{"role": "system", "content": system_prompt}] + self.get_normalized_history(history) + [{"role": "user", "content": user_prompt}]
 
         response = self.openai.chat.completions.create(model="gpt-4o-mini", messages=messages)
 
@@ -19,8 +25,27 @@ class HREvaluatorService():
 
         return response.choices[0].message.content
 
-    def get_company_profile(self) -> str:
+    def get_system_prompt(self) -> str:
         with open("./resources/profile.md", "r") as file:
-            content = file.readlines()
+            content = "\n".join(file.readlines())
 
-            return content
+            return Prompt.get_system_prompt(content)
+
+    def get_user_message(self, message):
+        if message["files"] is not None and len(message["files"]) > 0:
+            file_path = message["files"][0]
+
+            convertor = DocumentConverter()
+
+            result = convertor.convert(file_path)
+
+            return result.document.export_to_markdown()
+        else:
+            return message["text"]
+
+    def get_normalized_history(self, histories):
+        for history in histories:
+            if isinstance(history['content'], tuple):
+                history['content'] = history['content'][0]
+
+        return histories;
